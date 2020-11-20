5a73c30d2e60fec22cfb2847270ce78058084d9c
excing <courage.exc@gmail.com>
2020-11-20 16:49:28 +0800

    Update PostService(), Use database instead of map

--- service.go
+++ service.go
@@ -1,107 +1,54 @@
 package main
 
 import (
-	"errors"
-
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
-// 授权状态：等待中
-const statusAuthorizationWaiting = 0
+// Service basic information of restful api service
+type Service struct {
+	gorm.Model
+	ServiceID    string
+	ServiceName  string
+	ServiceDesc  string
+	ServiceToken string
+}
 
-// 授权状态：同意授权
-const statusAuthorizationUserGrant = 1
-
-// 授权状态：拒绝授权
-const statusAuthorizationUserDeny = -1
-
-const timeoutTokenExpire = 604800 // 授权有效期为 7 天
-const timeoutTokenDelete = 259200 // 授权失效后，保留 UserToken 3 天
-
-// ServiceInfo 服务信息
-type ServiceInfo struct {
-	// 服务 ID
-	ServiceID string
-	// 服务名
-	ServiceName string
-	// 服务详细，可选
+type serviceForm struct {
+	ServiceID   string `schema:"serviceId,required"`
+	ServiceName string `schema:"serviceName,required"`
 	ServiceDesc string
-	// 服务 Token 的 crypto/bcrypt 值
-	ServiceTokenEncode string
 }
-
-// AuthorizationInfo 认证请求信息
-// 请求用户将 ServiceProvider 服务授权给 ServiceRequester
-type AuthorizationInfo struct {
-	// 用户名
-	UserName string
-	// 用户 Token 的 crypto/bcrypt 值
-	UserTokenEncode string
-	// 提供方服务ID
-	ServiceProviderID string
-	// 请求方服务ID
-	ServiceRequesterID string
-	// 服务授权状态
-	// see: STATUS_AUTHORIZATION_WAITING, STATUS_AUTHORIZATION_USER_GRANT, STATUS_AUTHORIZATION_USER_DENY
-	AuthorizationStatus int
-
-	// 用户 Token 创建时间
-	TokenCreateTime int64
-	// 用户 Token 刷新时间
-	TokenUpdateTime int64
-	// 用户 Token 过期时间
-	TokenExpireTime int64
-	// 用户 Token 将被删除的时间
-	TokenDeleteTime int64
-}
-
-// 授权请求列表. key 为 authorizationCode, value 为用户 Token 的用户信息, 包含授权状态
-var authorizationMap map[string]AuthorizationInfo
-
-// 服务提供者列表
-var serviceMap map[string]ServiceInfo
-
-// 请求授权邮件列表
-var authorizationEmailMap map[string]string
 
 // PostServicer 提交服务注册
 func PostServicer(c *Context) error {
-	serviceID := c.PostForm("serviceId")
-	serviceName := c.PostForm("serviceName")
-	serviceDesc := c.PostForm("serviceDesc")
-
-	if "" == serviceID {
-		return c.BadRequest("serviceId is empty")
-	} else if "" == serviceName {
-		return c.BadRequest("serviceName is empty")
+	var form serviceForm
+	err := c.ParseForm(&form)
+	if err != nil {
+		return c.BadRequest(err.Error())
 	}
 
-	var providerRegisterResult, requesterRegisterResult = false, false
+	var service Service
+	err = db.Where("serviceId=?", form.ServiceID).Find(&service).Error
 
-	registerResult := make(map[string]string)
-
-	if _, ok := serviceMap[serviceID]; !ok {
-		token, encodeToken, err := "", "", errors.New("")
-
-		if err != nil {
-			return c.InternalServerError("registration failed: %s", err.Error())
-		}
-
-		serviceMap[serviceID] = ServiceInfo{
-			serviceID,
-			serviceName,
-			serviceDesc,
-			encodeToken,
-		}
-		registerResult["providerToken"] = token
-
-		providerRegisterResult = true
+	if err == nil && 0 != service.ID {
+		return c.Created("This service is already registered")
 	}
 
-	if providerRegisterResult || requesterRegisterResult {
-		return c.Ok(registerResult)
+	token, _ := New64BitUUID()
+
+	service.ServiceID = form.ServiceID
+	service.ServiceName = form.ServiceName
+	service.ServiceDesc = form.ServiceDesc
+	service.ServiceToken = token
+
+	err = db.Create(&service).Error
+
+	if err != nil {
+		return c.InternalServerError(err.Error())
 	}
-	return c.Created("This service is already registered")
+
+	return c.Ok("Post successed")
 }
 
 // DeleteServicer 注销服务
