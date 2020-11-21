a59b33afb9d1c544c1921436aaa485977a8202ff
excing <courage.exc@gmail.com>
2020-11-21 12:47:50 +0800

    gin unconditionally supports cross-domain requests

--- main.go
+++ main.go
@@ -66,7 +66,18 @@ func main() {
 
 	router := gin.Default()
 	router.SetHTMLTemplate(tmpl)
-	router.GET("/", func(c *gin.Context) {})
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, ResponseType, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 
 	v1 := router.Group("/v1")
 	v1.POST("/user/main/code", inout(PostMainCode))
