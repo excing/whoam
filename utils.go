22fc1276e6548ef086b794383f13457d41e9aa26
excing <courage.exc@gmail.com>
2020-11-23 14:52:02 +0800

    Delete the genUUID method, the New64BitUUID method uses genRandCode as its internal implementation, and the'=' symbol in the dictionary is updated to'.'

--- utils.go
+++ utils.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	crand "crypto/rand"
-	"io"
 	"math/rand"
 	"regexp"
 	"time"
 	"unsafe"
-
-	"github.com/google/uuid"
 )
 
 // VerifyEmailFormat Verify email address
@@ -18,9 +14,7 @@ func VerifyEmailFormat(email string) bool {
 	return reg.MatchString(email)
 }
 
-const digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_="
-
-var rander = crand.Reader // random function
+const digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_."
 
 const (
 	letterIdxBits = 6                    // 6 bits to represent a letter index
@@ -59,72 +53,8 @@ func genRandCode(n int, bit ...int) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-func genUUID() uuid.UUID {
-	UUID, uerr := uuid.NewRandom()
-
-	if uerr != nil {
-		UUID = uuid.New()
-	}
-
-	return UUID
-}
-
 // New64BitUUID 获取 64进制 UUID, 10个数字+26个小写字母+26个大写字母+(=、_), 长度 24
 // generates a random UUID according to RFC 4122
 func New64BitUUID() (string, error) {
-	uuid := make([]byte, 18)
-	n, err := io.ReadFull(rander, uuid)
-	if n != len(uuid) || err != nil {
-		return "", err
-	}
-	// variant bits; see section 4.1.1
-	uuid[8] = uuid[8]&^0xc0 | 0x80
-	// version 4 (pseudo-random); see section 4.1.3
-	uuid[6] = uuid[6]&^0xf0 | 0x40
-
-	return uuidValueOf(uuid), nil
-}
-
-func uuidValueOf(b []byte) string {
-	carry := 6
-	size := 3
-
-	var temp []byte
-	var s [68]byte
-	sindex := 0
-	var start, end int
-
-	for {
-		if start == len(b) {
-			break
-		}
-		end = start + size
-		if len(b) < end {
-			end = len(b)
-		}
-		temp = b[start:end]
-		start = end
-		u := uint64(temp[len(temp)-1])
-		ucarry := 8
-		for i := len(temp) - 2; 0 <= i; i-- {
-			u |= uint64(temp[i]) << ucarry
-			ucarry += 8
-		}
-		i := 4
-		for {
-			if u <= 0 {
-				break
-			}
-
-			i--
-			s[sindex+i] = digits[u&63]
-			u >>= carry
-		}
-		for 0 < i {
-			i--
-			s[sindex+i] = digits[0]
-		}
-		sindex += 4
-	}
-	return string(s[0:sindex])
+	return genRandCode(64), nil
 }
