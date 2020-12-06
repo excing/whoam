20a66d474bbc8f063612976631044a0114cf54c8
excing <courage.exc@gmail.com>
2020-12-06 15:55:35 +0800

    Errata: time.Minute -> time.Hour

--- user.go
+++ user.go
@@ -20,9 +20,9 @@ const (
 </body>`
 )
 
-const timeoutUserVerification = 900               // 用户验证码有效时长: 15分钟
-const timeoutRefreshToken = 30 * 24 * time.Minute // user refresh token timeout: 30day
-const timeoutAccessToken = 7 * time.Minute        // user access token timeout: 5min
+const timeoutUserVerification = 900             // 用户验证码有效时长: 15分钟
+const timeoutRefreshToken = 30 * 24 * time.Hour // user refresh token timeout: 30day
+const timeoutAccessToken = 7 * time.Minute      // user access token timeout: 5min
 
 // User basic information, id, email and
 type User struct {
