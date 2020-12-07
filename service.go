cc4bb2a7c55124b4ddd95a53e687ee03472c2437
excing <courage.exc@gmail.com>
2020-12-07 22:01:03 +0800

    Removed gorm, use ent. The DeleteServicer method has been removed. When the whoam service is started, the whoam service is automatically registered.

--- service.go
+++ service.go
@@ -1,56 +1,33 @@
 package main
 
 import (
-	"strings"
-
-	"gorm.io/gorm"
+	"whoam.xyz/ent/method"
+	"whoam.xyz/ent/service"
 )
-
-// Service basic information of restful api service
-type Service struct {
-	gorm.Model
-	ServiceID string
-	Name      string
-	Token     string
-	Describe  string
-}
-
-// ServiceMethod service method
-type ServiceMethod struct {
-	gorm.Model
-	Name  string
-	Scope string
-	// Fields []ServiceMethodField
-}
-
-// ServiceMethodField service method field
-// type ServiceMethodField struct {
-// 	Name     string
-// 	Kind     string
-// 	Default  string
-// 	Describe string
-// 	Required string
-// }
 
 // InitService initialize service related business
 func InitService() {
-	db.AutoMigrate(&Service{}, &ServiceMethod{})
-}
+	if ok, err := client.Service.Query().Where(service.IDEQ(MainServiceID)).Exist(ctx); !ok || err != nil {
+		_, err = client.Service.Create().
+			SetID(MainServiceID).
+			SetName("whoam service").
+			SetSubject("Support OAuth authorization, support service registration, support RAS.").
+			SetDomain("https://www.whoam.xyz").
+			SetCloneURI("https://github.com/excing/whoam.git").
+			Save(ctx)
 
-// ServiceAuthorize service authorize
-func ServiceAuthorize(c *Context, service *Service) bool {
-	token := c.GetHeader("Authorization")
-	if db.Where("token=?", token).Find(service).Error != nil || 0 == service.ID {
-		return false
+		if err != nil {
+			panic(err)
+		}
 	}
-
-	return true
 }
 
 type postServiceForm struct {
 	ServiceID   string `schema:"serviceId,required"`
 	ServiceName string `schema:"serviceName,required"`
 	ServiceDesc string
+	Domain      string `schema:"domain,required"`
+	CloneURI    string `schema:"cloneUri,required"`
 }
 
 // PostServicer 提交服务注册
@@ -61,68 +38,62 @@ func PostServicer(c *Context) error {
 		return c.BadRequest(err.Error())
 	}
 
-	var service Service
-	if err = db.Where("service_id=?", form.ServiceID).Find(&service).Error; err == nil && 0 != service.ID {
-		return c.Created("This service is already registered")
-	}
-
-	token := New64BitID()
-
-	service.ServiceID = form.ServiceID
-	service.Name = form.ServiceName
-	service.Describe = form.ServiceDesc
-	service.Token = token
-
-	err = db.Create(&service).Error
-	if err != nil {
-		return c.InternalServerError(err.Error())
-	}
-
-	return c.Ok(token)
-}
-
-type deleteServiceForm struct {
-	ServiceID    string `schema:"serviceId,required"`
-	ServiceToken string `schema:"serviceToken,required"`
-}
-
-// DeleteServicer 注销服务
-func DeleteServicer(c *Context) error {
-	var service Service
-	if !ServiceAuthorize(c, &service) {
-		return c.Unauthorized("Invalid token, please refresh the access token with Referh_token")
-	}
-
-	err := db.Delete(&service).Error
-	if err != nil {
-		return c.InternalServerError(err.Error())
-	}
+	_, err = client.Service.Create().
+		SetID(form.ServiceID).
+		SetName(form.ServiceName).
+		SetSubject(form.ServiceDesc).
+		SetDomain(form.Domain).
+		SetCloneURI(form.CloneURI).
+		Save(ctx)
 
 	return c.NoContent()
 }
 
+type serviceMethodBody struct {
+	Name    string `json:"name"`
+	Route   string `json:"route"`
+	Subject string `json:"subject"`
+	Scope   string `json:"scope"`
+}
+
 // PostServiceMethod receive service method submission
 func PostServiceMethod(c *Context) error {
-	var service Service
-	if !ServiceAuthorize(c, &service) {
-		return c.Unauthorized("Invalid token, please refresh the access token with Referh_token")
-	}
+	service, err := client.Service.Query().
+		Where(service.IDEQ(c.Param("id"))).
+		Only(ctx)
 
-	methods, err := c.FormArray("methods")
 	if err != nil {
 		return c.BadRequest(err.Error())
 	}
 
-	serviceMethods := make([]ServiceMethod, len(methods))
-	for i, method := range methods {
-		ss := strings.Split(method, ":")
-		name := ss[0]
-		scope := ss[1]
-		url := service.ServiceID + name
-		serviceMethods[i] = ServiceMethod{Name: url, Scope: scope}
+	var body []*serviceMethodBody
+
+	err = c.ShouldBindJSON(&body)
+	if err != nil {
+		return c.BadRequest(err.Error())
 	}
 
-	if err = db.Create(&serviceMethods).Error; err != nil {
+	tx, err := client.Tx(ctx)
+	if err != nil {
+		return c.InternalServerError(err.Error())
+	}
+
+	for _, m := range body {
+		_, err = tx.Method.Create().
+			SetName(m.Name).
+			SetRoute(m.Route).
+			SetSubject(m.Subject).
+			SetScope(method.Scope(m.Scope)).
+			SetOwner(service).
+			Save(ctx)
+
+		if err != nil {
+			return c.InternalServerError(err.Error())
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
 		return c.InternalServerError(err.Error())
 	}
 
