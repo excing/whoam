37929da22ea9d6f202e0bb5fab6a46bed6490179
excing <courage.exc@gmail.com>
2020-12-09 23:09:15 +0800

    Add `accord` table

--- ent/schema/accord.go
+++ ent/schema/accord.go
@@ -1,6 +1,10 @@
 package schema
 
-import "github.com/facebook/ent"
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
 
 // Accord holds the schema definition for the Accord entity.
 type Accord struct {
@@ -9,10 +13,14 @@ type Accord struct {
 
 // Fields of the Accord.
 func (Accord) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("name"),
+	}
 }
 
 // Edges of the Accord.
 func (Accord) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("articles", Article.Type).Required(),
+	}
 }
